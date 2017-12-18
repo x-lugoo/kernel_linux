c73ba98c8519b3727125b10c843a37f6dba61aaf
x-lugoo <2429753441@qq.com>
2017-12-18 17:44:36 +0800

    add a handling when no network interface card

--- network/getlocalip.c
+++ network/getlocalip.c
@@ -2,6 +2,7 @@
 #include<sys/types.h>
 #include<ifaddrs.h>
 #include<stdlib.h>
+#include<string.h>
 #include<arpa/inet.h>  
 
 int main(void)
@@ -16,12 +17,15 @@ int main(void)
 	addrTmp = addr;
 	while(addr){
 		tmp = (struct sockaddr_in*)(addr->ifa_addr);
-		if((0 == strcmp(addr->ifa_name,"eth0")) && (AF_INET == tmp->sin_family))
+		if(tmp != NULL)
 		{
-			printf("client address = %s\n", inet_ntop(AF_INET, &tmp->sin_addr, ipAddress,INET_ADDRSTRLEN));
-			break;
-
-		}	
+			if((0 == strcmp(addr->ifa_name,"eth0")) && (AF_INET == tmp->sin_family))
+			{
+				printf("client address = %s\n", inet_ntop(AF_INET, &tmp->sin_addr, ipAddress,INET_ADDRSTRLEN));
+				break;
+			}	
+			printf("family=%d name=%s\n",tmp->sin_family,addr->ifa_name);
+		}
 		addr = addr->ifa_next;
 	};
 	freeifaddrs(addrTmp);
