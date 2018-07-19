376ef5df08fb4c5ce6f3a58f198dd42ef3137f15
x-lugoo <2429753441@qq.com>
2018-07-19 18:32:35 +0800

    modify userlist.c

--- edu/list/userlist.c
+++ edu/list/userlist.c
@@ -3,11 +3,51 @@
 #include <stddef.h> /*notice : add containof()*/
 #include "list.h"
 
+#define EVALID 1 
+#define EPARAM 2
+
 struct player_entry{
 	struct list_head list;
 	int player_id;
 	unsigned short score;
 };
+
+struct jeff {
+	void (*probe)(struct jeff*);
+	char *name;
+};
+
+struct jeff_parent{
+	struct jeff j;
+	void (*probe)(struct jeff_parent *);
+	char *pname;
+};
+
+#define NONE "JEFF_NONE"
+
+void jeff_probe(struct jeff *j)
+{
+	struct jeff_parent *parent;
+
+	if(!j)
+		return ;
+	parent = container_of(j,struct jeff_parent,j);
+	if(!parent)
+		return;
+
+	if(parent->probe){
+		parent->pname = NONE;
+		parent->probe(parent);
+	}
+}
+
+
+void jeff_parent_probe(struct jeff_parent *parent)
+{
+	printf("jeff's parent name = %s\n",parent->pname);
+}
+
+	
 
 static LIST_HEAD(play_head);
 
@@ -34,11 +74,20 @@ static int mylist_init(void)
 	return 0;
 }
 
+struct jeff_parent jparent = {
+	.probe = jeff_parent_probe,
+	.j     = {
+		.probe =  jeff_probe,
+	},
+};
+
 
 		
 int main(void)
 {
-	mylist_init();
+
+//	mylist_init();
+	jparent.j.probe(&jparent.j);
 	return 0;
 }
 
